ae4c86075a9223536ace7418416aa3eff15475e6
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-06 09:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+Array.prototype.myMap = function (callback) {
+  // Write your code here.
+};
+
+Array.prototype.myFilter = function (callback) {
+  // Write your code here.
+};
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  // Write your code here.
+};
